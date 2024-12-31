24be16c29210b9f148718e5d662f5757d7d844bf
C-arl-os <112126461+C-arl-os@users.noreply.github.com>
2024-12-31 13:24:55 -0600

    final

--- Proyecto 2/proy/Clase15/Frontend/app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.Form):
-    iduser = forms.CharField(label='iduser')
-    password = forms.CharField(widget=forms.PasswordInput(), label='password')
-
-class FileForm(forms.Form):
-    file = forms.FileField(label='file')
